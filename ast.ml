0b1a2c22322256a8be90109dc67fd26e6d35ac43
FOUQUET aurelie <aurfouquet@gmail.com>
2022-05-26 00:56:18 +0200

    mise en place de l'interpreteur

--- ast.ml
+++ ast.ml
@@ -1,19 +1,17 @@
-type lettre = char
+type inputsymbols = Inputsymbols of char list
 
-type inputsymbols = Inputsymbols of lettre list
+type stacksymbols = Stacksymbols of char list
 
-type stacksymbols = Stacksymbols of lettre list
+type states = States of char list
 
-type states = States of lettre list
+type initialstate = Initialstate of char
 
-type initialstate = Initialstate of lettre
+type initialstack = Initialstack of char
 
-type initialstack = Initialstack of lettre
-
-type stack = Stack of lettre list
+type stack = char list
 
 type transitions = 
-  Transitions of (lettre * lettre * lettre * lettre * stack) list
+  Transitions of (char * char * char * char * stack) list
 
 type declarations =
   Declarations of inputsymbols * stacksymbols * states * initialstate * initialstack
@@ -21,35 +19,9 @@ type declarations =
 type automate = 
   Automate of declarations * transitions
 
+
+
+  
 let as_string = function
   | Automate (d,t) -> "HELLO"
-
-(* 
-let rec as_string = function
-  | Inputsymbols ()
-  | False -> "false"
-  | Or (l, r) -> apply "\\/" l r
-  | And (l, r) -> apply "/\\" l r
-  | Let (x, e1, e2) -> apply_let x e1 e2
-  | Let_declaration (x, e) -> apply_let_declaration x e
-  | Let_list (l, r) -> apply_let_list l r
-
-and apply op l r = 
-  "(" ^ as_string l ^ ") " ^ op ^ " (" ^ as_string r ^ ")"
-
-and apply_let variable expr1 expr2 =
-  "Let " ^ variable ^ " = " ^ as_string expr1 ^ " in " ^ as_string expr2
-
-and apply_let_declaration variable expr =
-  variable ^ " = " ^ as_string expr
-
-and apply_let_list l r =
-  "Let " ^ apply_list l ^ " in " ^ as_string r
-
-and apply_list l =
-  match l with 
-  | [] -> ""
-  | h::q ->
-    if q <> [] then as_string h ^ " and " ^ apply_list q
-    else as_string h *)
 
