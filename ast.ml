edad5c713a910324778c15029fb102318727f892
FOUQUET aurelie <aurfouquet@gmail.com>
2022-05-28 00:16:39 +0200

    ajout print ast

--- ast.ml
+++ ast.ml
@@ -20,33 +20,41 @@ type declarations =
 type automate = 
   Automate of declarations * transitions
 
-(*   
-let rec as_string = function
-  | Inputsymbols l -> "inupts symbols: " ^ parcoursListe l
-  | Stacksymbols l -> "stack symbols: " ^ parcoursListe l
-  | States l -> "states: " ^ parcoursListe l
-  | Initialstate a -> "initial state: " ^ a
-  | Initialstack a -> "initial stack symbol:" ^ a
-  | Stack l -> "stack" ^ parcoursListe l 
-  | Transitions l -> "transitions" ^ parcoursListeTrans l
-  | Declarations (i,stck,st,initst,initstck) -> "declarations: " ^ as_string i ^ as_string stck ^ as_string initst ^ as_string initstck
-  | Automate (d,t) -> "automate: " ^ as_string d ^ as_string t
-
-
-and parcoursListe l =
+(* let rec parcoursListe l =
   match l with
   | [] -> ""
-  | t::q ->   t ^  string ", " ^ parcoursListe q
+  | t::q ->   t ^   ", " ^ parcoursListe q
 
-and parcoursListeTrans l =
+let rec parcoursListeStack l =
   match l with
   | [] -> ""
-  | t::q -> "(" ^  t ^  string "," ^ parcoursListeTrans q ^ ")" *)
+  | t::q -> t ^ ";" ^ parcoursListeStack q
 
-(*
- and parcoursListeTrans l =
+  let rec parcoursListeTrans l = 
+    match l with
+    | [] -> ""
+    | t::q -> ";"^t^parcoursListeTrans q
+
+
+let as_string_inputSymb l =  "input symbols: " ^ parcoursListe l
+
+let as_string_stackSymb l = "stack symbols: " ^ parcoursListe l
+
+let as_string_state l = "states: " ^ parcoursListe l
+
+let as_string_initStack a = "initial stack: " ^ a
+
+let as_string_initState a = "initial state:" ^ a
+
+let as_string_stack l = parcoursListeStack l 
+
+let as_string_declaration (i,stck,st,initst,initstck) = as_string_inputSymb i ^"\n"^ as_string_stackSymb stck 
+  ^"\n"^ as_string_state st ^"\n"^ as_string_initState initst ^"\n"^ as_string_initStack initstck^"\n"
+
+let as_string_transition l = 
   match l with
   | [] -> ""
-  | t::q -> "(" ^ string t ; string "," ; parcoursListeTrans q ^ ")" 
- *)
+  | t::q -> "transitions: \n"^t^parcoursListeTrans q
 
+let as_string_automate (declaration,transitions) = "automate: " ^ as_string_declaration declaration 
+  ^ as_string_transition transitions *)
